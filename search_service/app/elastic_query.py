fe6ca0e99af7b90461bb919fd6355d172271ef16
unknown <taktuk07@gmail.com>
2026-02-26 12:24:07 +0200

    build ElastikQueryService class

--- search_service/app/elastic_query.py
+++ search_service/app/elastic_query.py
@@ -2,12 +2,9 @@ import streamlit as st
 import pandas as pd
 from elasticsearch import Elasticsearch
 import os
+from streamlit_autorefresh import st_autorefresh
 
-data = {'weapons': ['Sword', 'Bow', 'Axe'], 'data': [1, 3, 2]}
-df = pd.DataFrame(data)
-
-st.bar_chart(df.set_index('weapons'))
-es = Elasticsearch(os.getenv('ELASTIC_URI',"http://elasticsearch:9200"))
+es = Elasticsearch(os.getenv('ELASTIC_URI', "http://localhost:9200"))
 class ElastikQueryService:
     def get_wepons(self):
         query = {
@@ -15,9 +12,44 @@ class ElastikQueryService:
   "aggs": {
     "list_of_items": {
       "terms": {
-        "field": "weapons.keyword",
-        "size": 100 
+        "field": "weapons",
+        "size": 100
       }
     }
   }
 }
+        return es.search(index='image', body=query)
+    
+    def streamlit_get_wepons(self, result: dict):    
+          wepons_list = []
+          count = []
+          for item in result['aggregations']['list_of_items']['buckets']:
+              wepons_list.append(item['key'])
+              count.append(item['doc_count'])
+          data = {'weapons':wepons_list, 'data':count}
+          df = pd.DataFrame(data)
+          st.bar_chart(df.set_index('weapons'))
+
+    def get_most_common_words(self):
+        query = {
+  "size": 0,
+  "aggs": {
+    "list_of_items": {
+      "terms": {
+        "field": "10_commend_words",
+        "size": 100
+      }
+    }
+  }
+}
+        return es.search(index='image', body=query)
+    
+    def streamlit_get_common_words(self, result: dict):
+        words_list = []
+        count = []
+        for item in result['aggregations']['list_of_items']['buckets']:
+              words_list.append(item['key'])
+              count.append(item['doc_count'])
+        data = {'words': words_list, 'data':count}
+        df = pd.DataFrame(data)
+        st.bar_chart(df.set_index('words'))
